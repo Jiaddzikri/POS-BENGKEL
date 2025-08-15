31ad1940f20b46b2b468d187e6f4579421bfeb50
jiaddzikri <jiaddzikri140604@gmail.com>
2025-08-15 10:36:42 +0700

    refactor(inventory): make inventory service more modular

--- app/Service/Inventory/InventoryService.php
+++ app/Service/Inventory/InventoryService.php
@@ -5,6 +5,7 @@ namespace App\Service\Inventory;
 use App\Models\ItemRecord;
 use App\Models\VariantItem;
 use App\Request\AdjustStockRequest;
+use Carbon\Carbon;
 use DB;
 use Exception;
 
@@ -69,5 +70,50 @@ class InventoryService
       DB::rollBack();
       throw $e;
     }
+  }
+
+  private function baseStockMovementQuery(string $tenantId, $filters = [])
+  {
+    $baseQuery = ItemRecord::with('variant.item.category')
+      ->whereHas('variant.item', function ($query) use ($tenantId) {
+        $query->where('tenant_id', $tenantId)
+          ->where('is_deleted', false);
+      });
+
+    if (isset($filters["searchStockMovement"])) {
+      $baseQuery->where(function ($query) use ($filters) {
+        $query->whereHas('variant', function ($varianQuery) use ($filters) {
+          $varianQuery->where('name', 'like', '%' . $filters["searchStockMovement"] . '%')
+            ->orWhere('sku', 'like', '%' . $filters["searchStockMovement"] . '%');
+        })->orWhereHas('variant.item', function ($itemQuery) use ($filters) {
+          $itemQuery->where('name', 'like', '%' . $filters["searchStockMovement"] . '%')
+          ;
+        })->orWhereHas('variant.item.category', function ($categoryQuery) use ($filters) {
+          $categoryQuery->where('name', 'like', '%' . $filters["searchStockMovement"] . '%');
+        });
+      });
+    }
+
+    if (isset($filters['startDate']) && isset($filters['endDate'])) {
+      $baseQuery->whereBetween('created_at', [Carbon::parse($filters['startDate'])->startOfDay(), Carbon::parse($filters['endDate'])->endOfDay()]);
+
+    }
+    return $baseQuery;
+  }
+
+  public function getStockMovementPaginated(string $tenantId, array $filters = [])
+  {
+    $query = $this->baseStockMovementQuery($tenantId, $filters);
+
+    $stockMovements = $query->latest()->paginate(10)->withQueryString();
+
+    return $stockMovements;
+  }
+
+  public function getStockMovementCount(string $tenantId, array $filters = [])
+  {
+    $query = $this->baseStockMovementQuery($tenantId, $filters);
+
+    return $query->count();
   }
 }
