fa2e17df0d1b059011ec612ab1b4061396a0ea74
Nyanta-23 <muhamadilhan02404@gmail.com>
2025-08-17 15:58:39 +0700

    fix: data need

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            TenantSeeder::class,
-            CategorySeeder::class
+            // TenantSeeder::class,
+            // CategorySeeder::class
         ]);
     }
 }
